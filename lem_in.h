25e966b653562370c3a9451b2dfb875211bd5160
Mohamed Bouzargtoun <mobouzar@e2r4p1.1337.ma>
2019-11-15 16:33:10 +0100

    done

--- lem_in.h
+++ lem_in.h
@@ -6,26 +6,41 @@
 /*   By: mobouzar <mobouzar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/14 13:49:39 by mobouzar          #+#    #+#             */
-/*   Updated: 2019/11/14 14:36:15 by mobouzar         ###   ########.fr       */
+/*   Updated: 2019/11/15 15:31:40 by mobouzar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LEM_IN_H
 # define LEM_IN_H
 
-# include <fcntl.h>
 # include "libft/libft.h"
+# include <fcntl.h>
 
-typedef struct		s_adlist
+typedef struct		s_point
+{
+	int				y;
+	int				x;
+}					t_point;
+
+typedef struct		s_map 
+{
+	char			*map;
+	struct s_map	*next;
+}					t_map;
+
+typedef struct		s_room
 {
 	char			*room;
-	struct s_adlist	*next;
-}					t_adlist;
+	t_point			cords;
+}					t_room;
 
 typedef struct		s_lem_in
 {
+	t_room			*room;
+	char			*comment;
 	int				nbants;
-	char			*comm;
 }					t_lem_in;
+
+int		get_nbrofants(t_lem_in *l, char *line);
 
 #endif
--- main.c
+++ main.c
@@ -6,21 +6,39 @@
 /*   By: mobouzar <mobouzar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/14 13:49:49 by mobouzar          #+#    #+#             */
-/*   Updated: 2019/11/14 14:42:53 by mobouzar         ###   ########.fr       */
+/*   Updated: 2019/11/15 16:32:29 by mobouzar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
-int		parse_data(t_lem_in *l)
+int		read_map(t_map *map)
 {
-	char	*line;
+	t_map		*m;
+	char		*line;
+	int			i;
+	
+	i = 0;
+	while (get_next_line(0, &line))
+	{
+		m = (t_map *)malloc(sizeof(t_map));
+		map->map = ft_strdup(line);
+		map = map->next;
+		ft_putendl("line");
+	}
+	map->next = NULL;
+	return (1);
+}
+
+int		get_nbrofants(t_lem_in *l, char *line)
+{
+	// char	*line;
 	int		len;
 	int		i;
 
 	i = 0;
 	l->nbants = -1;
-	get_next_line(0, &line);
+	// get_next_line(0, &line);
 	len = ft_strlen(line);
 	while (len <= 10 && line[i])
 		if (ft_isdigit(line[i]))
@@ -34,11 +52,30 @@ int		parse_data(t_lem_in *l)
 	return (1);
 }
 
+int		get_rooms(t_lem_in *l)
+{
+	char	*line;
+
+	while (get_next_line(0, &line))
+	{
+		if (ft_strstr(line, "##start"))
+		{
+			
+		}
+	}
+	return (1);
+}
+
 int	main(int ac, char **av)
 {
-	t_lem_in	l;
+	t_map		*m;
 
-	parse_data(&l);
-	ft_printf("%d\n", l.nbants);
+	m = (t_map *)malloc(sizeof(t_map));
+	read_map(m);
+	while (m->next != NULL)
+	{
+		ft_printf("%s\n", m->map);
+		m = m->next;
+	}
 	return (0);
 }
